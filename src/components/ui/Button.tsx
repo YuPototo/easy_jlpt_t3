c4b0ecc9ed7705f9e70c4e9ba5dfbd0fe225bd39
Yu Qin <sirius.qinyu@gmail.com>
2023-02-22 17:42:14 +0800

    refactor button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -6,59 +6,51 @@ const buttonClasses = cva(
   {
     variants: {
       intent: {
-        primary: [],
-        secondary: [],
-        danger: [],
+        primary: "",
+        secondary: "",
+        danger: "",
       },
       size: {
-        small: ["text-sm", "py-1", "px-2"],
-        medium: ["text-base", "py-2", "px-4"],
+        small: "text-sm py-1 px-2",
+        medium: "text-base py-2 px-4",
       },
       outline: {
-        true: ["border", "bg-white", "text-green-700"],
+        true: "border bg-white",
       },
     },
     compoundVariants: [
       {
         intent: "primary",
         outline: false,
-        class: [
-          "bg-green-600",
-          "text-white",
-          "border-green-600",
-          "hover:bg-green-700",
-          "hover:border-green-700",
-        ],
+        className:
+          "text-white bg-green-600 border-green-600 hover:bg-green-700 hover:border-green-700",
       },
       {
         intent: "primary",
         outline: true,
-        class: [
-          "border-green-600",
-          "hover:bg-green-100",
-          "bg-white",
-          "text-green-600",
-        ],
+        className: "text-green-600 border-green-600 hover:bg-green-100",
       },
       {
         intent: "secondary",
         outline: true,
-        class: [
-          "bg-white",
-          "text-gray-600",
-          "border-gray-600",
-          "hover:bg-gray-100",
-        ],
+        className: "text-gray-600 border-gray-600 hover:bg-gray-100",
+      },
+      {
+        intent: "secondary",
+        outline: false,
+        className:
+          "bg-gray-600 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-700",
       },
       {
         intent: "danger",
         outline: true,
-        class: [
-          "border-red-600",
-          "hover:bg-red-100",
-          "bg-white",
-          "text-red-600",
-        ],
+        class: "border-red-600 hover:bg-red-100 text-red-600",
+      },
+      {
+        intent: "danger",
+        outline: false,
+        class:
+          "bg-red-600 text-white border-red-600 hover:bg-red-700 hover:border-red-700",
       },
     ],
     defaultVariants: {
--- src/pages/ui/buttonPage.tsx
+++ src/pages/ui/buttonPage.tsx
@@ -38,6 +38,48 @@ const ButtonPage: NextPage = () => {
 
           <Button outline>按钮</Button>
         </div>
+
+        <div className="my-10">
+          <div>
+            intent: <code>secondary</code>
+          </div>
+
+          <Button intent="secondary">按钮</Button>
+        </div>
+
+        <div className="my-10">
+          <div>
+            intent: <code>secondary</code>
+          </div>
+          <div>
+            outline: <code>true</code>
+          </div>
+
+          <Button intent="secondary" outline>
+            按钮
+          </Button>
+        </div>
+
+        <div className="my-10">
+          <div>
+            intent: <code>danger</code>
+          </div>
+
+          <Button intent="danger">按钮</Button>
+        </div>
+
+        <div className="my-10">
+          <div>
+            intent: <code>danger</code>
+          </div>
+          <div>
+            outline: <code>true</code>
+          </div>
+
+          <Button intent="danger" outline>
+            按钮
+          </Button>
+        </div>
       </main>
     </>
   );
